fd081568c40f68d8b9c425d27174cca6391c138c
mjsipes <msipes@usc.edu>
2025-07-27 23:20:16 -0700

    w

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,18 @@ import PanoramaZoom from "@/components/PanoramaZoom";
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
+              <Image
+          loader={supabaseLoader as ImageLoader}
+          src={`mj-photos/home/small/pano.jpg`}
+          width={1600}
+          height={1000}
+          quality={90}
+          alt="Switzerland Pano"
+          sizes="100vw"
+          className="w-full h-auto object-cover"
+          priority
+        />
       <PanoramaZoom />
-      {/* PhotoCarousel will be re-enabled later */}
     </div>
   );
 }
