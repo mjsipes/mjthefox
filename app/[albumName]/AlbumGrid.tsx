55f0b841ad3789d57d3d3024db8b6de41427c30d
mjsipes <msipes@usc.edu>
2025-07-28 23:19:48 -0700

    i think there is still a lot to play around with

--- app/[albumName]/AlbumGrid.tsx
+++ app/[albumName]/AlbumGrid.tsx
@@ -28,22 +28,6 @@ export default function AlbumGrid({
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [router]);
 
-  // useEffect(() => {
-  //   if (!albumImageMetadata.length) return;  
-  //   const preloadImage = (name: string) => {
-  //     const img = document.createElement("img");
-  //     img.src = supabaseLoader({
-  //       src: `mj-photos/${albumName}/large/${name}`,
-  //       width: 1600,
-  //       quality: 95,
-  //     });
-  //   };
-  
-  //   // Preload all full-size images (tweak if needed for performance)
-  //   albumImageMetadata.forEach(({ name }) => preloadImage(name));
-  // }, [albumImageMetadata, albumName]);
-  
-
   return (
     <div className="grid grid-cols-2 gap-4 p-4">
       {
@@ -60,7 +44,6 @@ export default function AlbumGrid({
               width={400}
               height={300}
               quality={95}
-              sizes="(max-width: 768px) 100vw, 50vw"
               className="object-cover w-full h-auto"
             />
           </Link>
--- utils/supabase/supabase-image-loader.js
+++ utils/supabase/supabase-image-loader.js
@@ -1,6 +1,7 @@
 const projectId = 'gjbeonnspjcwyrpgcnuz' // your supabase project id
 
 export default function supabaseLoader({ src, width, quality }) {
+  width = Math.min(width, 1600);
   width = 1600;
   return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&quality=${quality || 75}&resize=contain`
 }
