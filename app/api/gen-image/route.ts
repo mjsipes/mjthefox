75108dd871ed0b7c4d02ad7835ab4101176cc74d
mjsipes <msipes@usc.edu>
2025-08-17 22:02:20 -1000

    Add OpenAI and Supabase functionality for image generation and retrieval, restructuring imports for clarity.

--- app/api/gen-image/route.ts
+++ app/api/gen-image/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { openaiGenImage } from "./openai-gen-image";
-import { getSupabaseImages } from "./supabase-get-all-images";
 import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
+import { getSupabaseImages } from "./supabase-functions";
 import OpenAI from "openai";
+import { openaiGenImage } from "./openai-functions";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
--- app/api/gen-image/test.ts
+++ app/api/gen-image/test.ts
@@ -1,25 +1,24 @@
 // npx tsx --env-file=.env app/api/gen-image/test.ts "A tiny turtle on a surfboard"
 // node --env-file=.env --import=tsx app/api/gen-image/test.ts "A tiny turtle on a surfboard"
 
-import { createClient } from "@supabase/supabase-js";
-import { openaiGenImage } from "./openai-gen-image";
-import { getSupabaseImages } from "./supabase-get-all-images";
 import OpenAI from "openai";
+import { openaiGenImage } from "./openai-functions";
+import { createClient } from "@supabase/supabase-js";
+import { getSupabaseImages } from "./supabase-functions";
 
 const prompt = process.argv[2] || "World";
 
-// const supabase = createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-// );
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 // getSupabaseImages(supabase).then(console.log);
+async function hi(){
 
-const client = new OpenAI();
-openaiGenImage(prompt, client)
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  const { data, error } = await supabase.storage.getBucket("mj-photos");
+  console.log(data);
+}
+
+hi();
+// const client = new OpenAI();
+// openaiGenImage(prompt, client);
