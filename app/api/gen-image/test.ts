fcbff27bd5a65062a6aab852da53f3e432658c67
mjsipes <msipes@usc.edu>
2025-08-25 07:48:06 -1000

    some bs to get vercel ci/cd to pass

--- app/api/gen-image/test.ts
+++ app/api/gen-image/test.ts
@@ -5,20 +5,35 @@ import OpenAI from "openai";
 import { openaiGenImage } from "./openai-functions";
 import { createClient } from "@supabase/supabase-js";
 import { getSupabaseImages } from "./supabase-functions";
-
+import { SupabaseClient } from "@supabase/supabase-js";
+import { generate_image_with_responses } from "./openai-functions";
+import { images_as_input } from "./openai-functions";
+import { create_image } from "./openai-functions";
+import {create_image_edit} from "./openai-functions";
+import { uploadLocalJpgToSupabase } from "./supabase-functions";
 const prompt = process.argv[2] || "World";
-
+const client = new OpenAI();
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
-// getSupabaseImages(supabase).then(console.log);
-async function hi(){
 
+
+// getSupabaseImages(supabase).then(console.log);
+
+async function hi(){
   const { data, error } = await supabase.storage.getBucket("mj-photos");
   console.log(data);
 }
+// hi();
 
-hi();
-// const client = new OpenAI();
+async function url_to_id(url: string, supabase: SupabaseClient) {
+
+}
+async function id_to_url(id: string, supabase: SupabaseClient) {}
+
+// generate_image_with_responses(prompt, client);
+// images_as_input(client);
+// create_image(prompt, client);
+create_image_edit(client,supabase);
 // openaiGenImage(prompt, client);
