216a2f53cc706f5f1c298772eabe8063a71a6a93
mjsipes <msipes@usc.edu>
2025-08-28 14:28:04 -0700

    eslint

--- app/api/gen-image/route.ts
+++ app/api/gen-image/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
-import { getSupabaseImages } from "./supabase-functions";
 import OpenAI from "openai";
-import { openaiGenImage } from "./openai-functions";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -13,7 +11,6 @@ export async function GET(request: Request) {
   // const result = await openaiGenImage(prompt, client);
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
-  const result = await getSupabaseImages(supabase);
 
-  return NextResponse.json(result);
+  return NextResponse.json({ message: "Hello, world!" });
 }
