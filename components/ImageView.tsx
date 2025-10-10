54ec8c1cb747f4763eda36b558c3808c4872cc35
mjsipes <msipes@usc.edu>
2025-10-10 00:32:43 -0700

    lovoing the new style

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -68,21 +68,21 @@ export default function ImageView({
   }, [imageName, albumImageMetadata, albumName, router, navigateToImage]);
 
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-4 relative">
-      <div className="absolute left-0 top-0 w-1/3 h-full cursor-w-resize z-10" onClick={() => navigateToImage('prev')} 
-      />
-      <div className="absolute right-0 top-0 w-1/3 h-full cursor-e-resize z-10" onClick={() => navigateToImage('next')} 
-      />
-      <Image
-        loader={supabaseLoader as ImageLoader}
-        src={imageUrl || imagePath}
-        alt={`${imageName} from ${albumName}`}
-        width={1600}
-        height={1200}
-        className={`max-w-full max-h-[calc(100vh-6rem)] object-contain ${inverted ? 'invert' : ''}`}
-        quality={95}
-        priority
-      />
+    <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-4">
+      <div className="relative inline-block border border-transparent hover:border-sipes-green">
+        <Image
+          loader={supabaseLoader as ImageLoader}
+          src={imageUrl || imagePath}
+          alt={`${imageName} from ${albumName}`}
+          width={1600}
+          height={1200}
+          className={`max-w-full max-h-[calc(100vh-6rem)] object-contain ${inverted ? 'invert' : ''}`}
+          quality={95}
+          priority
+        />
+        <div className="absolute left-0 top-0 w-1/3 h-full cursor-w-resize z-10" onClick={() => navigateToImage('prev')} />
+        <div className="absolute right-0 top-0 w-1/3 h-full cursor-e-resize z-10" onClick={() => navigateToImage('next')} />
+      </div>
     </div>
   );
 }
