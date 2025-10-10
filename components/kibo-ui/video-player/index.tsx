d5218e5ab757dca1c41254c00bd40c276aea6f78
mjsipes <msipes@usc.edu>
2025-10-10 01:11:38 -0700

    w

--- components/kibo-ui/video-player/index.tsx
+++ components/kibo-ui/video-player/index.tsx
@@ -121,5 +121,5 @@ export const VideoPlayerContent = ({
   className,
   ...props
 }: VideoPlayerContentProps) => (
-  <video className={cn("mt-0 mb-0", className)} {...props} />
+  <video suppressHydrationWarning className={cn("mt-0 mb-0", className)} {...props} />
 );
